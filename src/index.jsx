cb4838a66a4e8aec5aaf8861a2de80a4181f6fa5
biplab malakar <biplab.x@venturepact.com>
2019-03-18 17:55:59 +0530

    routing, ant, redux added in project

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-// import AdminView from './app/admin/component';
-// import FrequencyComponent from './app/frequency/view';
 import AppRoute from './route-config';
 import { connect } from "react-redux";
 
 const mapStateToProps = state=> {return {baseHttpClass: state.baseHttpClass}}
 class RootHtml extends React.Component {
-    
+
     constructor(props) {
         super(props);
     }
